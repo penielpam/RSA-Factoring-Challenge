53e8fdd702454b3c2fbc0cffb71ff33ca319dc55
penielpam <penielpam@gmail.com>
2023-10-27 15:20:25 -0700

    RSA Factoring Challenge

---
+++ factor.h
@@ -0,0 +1,14 @@
+#ifndef FACTOR_H
+#define FACTOR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define _GNU_SOURCE
+
+/* Function declaration for factorize */
+int factorize(char *buffer);
+
+#endif // FACTOR_H
+
